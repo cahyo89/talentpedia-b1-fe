4e3a188214b8c2c453ca79b11bc3fe65d791f319
fnowrd <firyalnabilah28@gmail.com>
2022-03-15 19:19:25 +0700

    Terms and Condition

---
+++ term.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="content_container" >
+        <h5 class="title is-5">Persyaratan dan Ketentuan </h5>
+        <div class="tile">
+           <p>Selamat datang di TᗩᑭEᗪ'ᔕ Terima kasih telah mengunjungi website kami. 
+            Halaman ini, bersama dengan pedoman kami dan dokumen lain yang dirujuk menjelaskan 
+            kepada Anda mengenai persyaratan ketentuan (“Syarat dan Ketentuan”) terkait cara menggunakan 
+            Platform kami (sebagaimana didefinisikan di bawah) baik sebagai tamu atau sebagai Pelanggan. 
+            Harap baca Syarat dan Ketentuan ini dengan baik sebelum Anda memesan Produk dari Platform. 
+            Dengan menggunakan Platform kami berarti Anda telah menyetujui untuk terikat pada Syarat dan 
+            Ketentuan ini dan untuk mematuhinya.</p> 
+        </div>
+        <br>
+      
+        <div class="tile is-ancestor">
+            <div class="tile is-parent">
+                <article class="tile is-child">
+                    <aside class="menu">
+                        <p class="menu-label">
+                            BANTUAN
+                        </p>
+                        <ul class="menu-list">
+                            <li></li>
+                            <li>
+                            <ul>
+                                <li><a href="/term">Terms and Conditions</a></li>
+                                <li><a href="/privacy">Privacy</a></li>
+                                 <li><a href="/quality">Quality</a></li>
+                            </ul>
+                            </li>
+                        </ul>
+                    </aside>
+                </article>
+            </div>
+            <div class="tile is-parent is-12">
+                <article class="tile is-child box">     
+                   <h6 class="title is-6">1. Tentang Kami</h6>
+                    Kami adalah peserta FrontEnd dan BackEnd Batch 1 dari TalentPedia.
+                    <br>
+                    <br>
+                    TᗩᑭEᗪ'ᔕ adalah akronim dari TalentPedia Shop yang bertujuan untuk menampilkan
+                    <p />hasil karya dari peserta FE dan BE Batch 1.
+                   <p /> Website ini mampu menunjukkan hasil pembelajaran yang telah kami dapatkan.
+                    <br>
+                    <br>
+                    <h6 class="title is-6">2. Persyaratan</h6>
+                    Untuk melakukan Pesanan di TᗩᑭEᗪ'ᔕ Anda harus berusia lebih dari 18 (delapan belas) tahun.
+                    <p /> Jika usia Anda di bawah 18 (delapan belas), Anda dapat melakukan Pesanan di TᗩᑭEᗪ'ᔕ 
+                    <p />dengan bantuan orangtua atau wali Anda.
+                    <br>
+                    <br>
+                    <h6 class="title is-6">3. Harga dan Biaya Pengiriman</h6>
+                    Biaya pengiriman untuk setiap pesanan akan didasari pada lokasi tujuan pengiriman, ukuran, 
+                    <p />dan berat pesanan Anda. Biaya untuk setiap metode pengiriman ditunjukkan dengan jelas 
+                    <p />selama proses check-out. Biaya pengiriman standar kami bervariasi berdasarkan lokasi tujuan pengiriman. 
+                    <p />Jika kami tidak dapat mengirimkan pesanan Anda secara lengkap dan harus melakukan lebih dari satu pengiriman,
+                    <p />tidak akan ada biaya tambahan untuk pengiriman berikutnya. Harga di toko, katalog, dan online dapat bervariasi.
+                    <br>
+                    <br>
+                    <h6 class="title is-6">4. Pemesanan</h6>
+                    Opsi pemesanan yang tersedia untuk Anda adalah melalui TᗩᑭEᗪ'ᔕ.
+                    <br>
+                    <br>
+                    Setelah Anda telah melakukan pemesanan, jika Anda telah memberi kami alamat email Anda, 
+                    <p />Anda akan menerima email konfirmasi. Jika karena alasan apa pun kami tidak dapat 
+                    <p />memenuhi pesanan Anda, kami akan memberitahu Anda secepatnya. Jika kami telah menerima 
+                    <p />pembayaran untuk pesanan tersebut, kami akan berusaha mengembalikan jumlah yang berlaku
+                    <p />menggunakan metode yang sama dengan yang digunakan untuk melakukan pembayaran. 
+                    <p />Jika, karena alasan apa pun, pengaturan alternatif diperlukan, perwakilan TᗩᑭEᗪ'ᔕ akan
+                    <p /> menghubungi Anda untuk menyelesaikan pengembalian dana.       
+                    <br>
+                    <br>
+                    <h6 class="title is-6">5. Pengiriman</h6>
+                    TᗩᑭEᗪ'ᔕ melakukan pengecekan secara lengkap dalam menerima dan memproses pesanan dan akan berusaha
+                    <p />mengirimkan pesanan Anda ke alamat di Indonesia, sesuai dengan opsi pengiriman yang Anda pilih.
+                    <br>
+                    <br>
+                    TᗩᑭEᗪ'ᔕ berupaya untuk mengirimkan pesanan dalam waktu secepat mungkin sesuai urutan antrean. 
+                    <p />Kami berusaha untuk mengirimkan barang dalam waktu 2 hingga 3 hari setelah konfirmasi pesanan,
+                    <p />kecuali terdapat kesepakatan lain. Silakan tunggu selama 8-10 hari kerja untuk pengiriman ke luar
+                    <p />Pulau Jawa. Meskipun kami berusaha untuk mengirimkan dalam kerangka waktu yang dikomunikasikan, 
+                    <p />pengiriman mungkin memakan waktu lebih lama karena peristiwa yang tidak terduga. Dalam hal yang tidak
+                    <p />terduga, jika waktu pengiriman lebih dari 30 hari, Anda dapat membatalkan pesanan Anda.
+                    <br>
+                    <br>
+                    Sebelum Anda melakukan pemesanan, Anda akan diberitahu tentang rincian perkiraan pengiriman. 
+                    <p />Setelah Anda melakukan pemesanan, maka Anda akan menerima konfirmasi email mengenai
+                    <p />perkiraan pengiriman pesanan Anda.
+                    <br>
+                    <br>
+                   
+                </article>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    body{
+    font-family: Apercu,Gill Sans MT,Gill Sans,Arial,sans-serif;
+    color: #000;
+    margin: 0;
+    }
+
+    .content_container {
+    letter-spacing: 0.2em;
+    font-size: 12px;
+    padding: 16px 100px 100px 100px;
+    line-height: 150%;
+    margin: 40px;
+    }
+
+    i{
+        font-style: italic;
+    }
+</style>
